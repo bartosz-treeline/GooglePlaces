3af0505b9369ebada2de2078b375a06d6c61f068
Bartosz Olszanowski <bartosz.olszanowski@treelineinteractive.com>
2022-06-15 15:01:14 +0200

    remove static

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GooglePlaces",
     products: [
-        .library(name: "GooglePlaces", type: .static, targets: ["GooglePlacesTarget"]),
+        .library(name: "GooglePlaces", targets: ["GooglePlacesTarget"]),
     ],
     dependencies: [
 
